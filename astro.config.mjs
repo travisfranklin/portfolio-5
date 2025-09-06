706f6374a4645f92a4f10a0cb487998eb30bbafb
Travis Franklin <travislf88@gmail.com>
2025-09-06 08:53:29 -0700

    Restore Tailwind CSS Vite plugin - build issues are now fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://travisfranklin.github.io/portfolio-5',
+    vite: {
+        plugins: [tailwindcss()]
+    },
     integrations: [mdx(), sitemap()]
 });
